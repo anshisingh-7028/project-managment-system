f84f4bb359855b30b879d24ec2764988ad53c1f2
Anshika Singh <anshikas00449@gmail.com>
2026-07-18 00:30:41 +0530

    Update backend CORS and deployment config

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const io = new Server(server, {
 cors:{
    origin:[
     "http://localhost:5173",
-    "https://project-managment-system-three.vercel.app",
+    "https://project-managment-system-818y.vercel.app",
     
    ],
    methods:[
